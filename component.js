d38570904b1af94b56aed3ab03fe59abf8fc99fd
LNSY <41439429+lindseymysse@users.noreply.github.com>
2021-05-20 17:50:47 -0700

    formatted comments

--- component.js
+++ component.js
@@ -32,9 +32,11 @@ you don't know how to actually write javascript 🤷.
 
 class HansElement extends HTMLElement{
   connectedCallback(){
-    // this function runs when the component is connected to the dom. 
-    // the this keyword refers to the HTML component itself, and you
-    // can treat it as any DOM object
+    /* 
+      this function runs when the component is connected to the dom. 
+      the this keyword refers to the HTML component itself, and you
+      can treat it as any DOM object 
+    */
     this.innerHTML = '<h1>Hans Element</h1>'
   }
   
@@ -44,21 +46,25 @@ class HansElement extends HTMLElement{
   }
 
   attributeChangedCallback(name, old_value, new_value){
-    // what to do when an attribute has changed
-    // name is the attribute name that has changed
-    // old_value is the old name of the attribute
-    // new_value is the new name of the attribute
+    /*
+      what to do when an attribute has changed
+      name is the attribute name that has changed
+      old_value is the old name of the attribute
+      new_value is the new name of the attribute
     
-    // To change update this element, access it like any other
-    // HTML element and use setAttribute() to make changes.
-    // an example:
-    // document.querySelector('hans-element').setAttribute('message', 'Hello World')
+      To change update this element, access it like any other
+      HTML element and use setAttribute() to make changes.
+      an example:
+      document.querySelector('hans-element').setAttribute('message', 'Hello World')
+    */
   }
 }
 
-// this component can be placed in the document using the notation
-// <hans-element name="element name here"></hans-element>
-// to change the name of the element in the dom, change the 
-// value in the quotation marks. 
+/*
+  this component can be placed in the document using the notation
+  <hans-element name="element name here"></hans-element>
+  to change the name of the element in the dom, change the 
+  value in the quotation marks. 
+*/
 
 customElements.define('hans-element', HansElement)
